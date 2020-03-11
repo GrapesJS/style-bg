fc4ed7f4c2975af0007c0ab41d540704deeb0ee8
Artur Arseniev <artur.catch@hotmail.it>
2020-03-11 22:12:46 +0100

    Initial commit 🍇

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export default (editor, opts = {}) => {
+  const options = { ...{
+    i18n: {},
+    // default options
+  },  ...opts };
+
+  // TODO Remove
+  editor.on('load', () =>
+    editor.addComponents(
+        `<div style="margin:100px; padding:25px;">
+            Content loaded from the plugin
+        </div>`,
+        { at: 0 }
+    ))
+};
---
+++ src/locale/en.js
@@ -0,0 +1,5 @@
+export default {
+    'grapesjs-style-bg': {
+        // 'key': 'value',
+    },
+};
