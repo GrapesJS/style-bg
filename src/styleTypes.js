f4619f76404bca48950796406193ae32089e6cba
Artur Arseniev <artur.catch@hotmail.it>
2020-03-18 02:35:52 +0100

    Add svg icons to bg type radio

--- src/styleTypes.js
+++ src/styleTypes.js
@@ -1,15 +1,17 @@
 // Types for image background
 import { typeBgKey } from './utils';
 
+const  bgTypeStyle = 'style="max-height: 16px; display: block" viewBox="0 0 24 24"';
+
 export const typeBg = {
   name: ' ',
   property: typeBgKey,
   type: 'radio',
   defaults: 'img',
   options: [
-    { value: 'img' },
-    { value: 'color' },
-    { value: 'grad' },
+    { value: 'img', name: `<svg ${bgTypeStyle}><path fill="currentColor" d="M8.5 13.5l2.5 3 3.5-4.5 4.5 6H5m16 1V5a2 2 0 00-2-2H5a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2z"/></svg>` },
+    { value: 'color', name: `<svg ${bgTypeStyle}><path fill="currentColor" d="M19 3H5a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2z"/></svg>` },
+    { value: 'grad', name: `<svg ${bgTypeStyle}><path fill="currentColor" d="M11 9h2v2h-2V9m-2 2h2v2H9v-2m4 0h2v2h-2v-2m2-2h2v2h-2V9M7 9h2v2H7V9m12-6H5a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2M9 18H7v-2h2v2m4 0h-2v-2h2v2m4 0h-2v-2h2v2m2-7h-2v2h2v2h-2v-2h-2v2h-2v-2h-2v2H9v-2H7v2H5v-2h2v-2H5V5h14v6z"/></svg>` },
   ],
 };
 
