4cb6a5f4b38319a566d76aad40b15cdcdaf7fe7d
Artur Arseniev <artur.catch@hotmail.it>
2022-11-30 15:13:05 +0100

    Refactor stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type grapesjs from 'grapesjs';
-import styleGradient, { PluginOptions as StyleGradientOptions, parseGradient } from 'grapesjs-style-gradient';
+import styleGradient, { PluginOptions as StyleGradientOptions, parseGradient, toGradient, getValidDir, GRAD_DIRS, GRAD_TYPES } from 'grapesjs-style-gradient';
 
 export interface PluginOptions {
   /**
@@ -27,12 +27,49 @@ const capitalize = (str: string) => {
   return str && str.charAt(0).toUpperCase() + str.substring(1);
 };
 
+const getLayerFromBgImage = (imagePart: string) => {
+  const result: Record<string, any> = {
+    [PROPERTY_BG_TYPE]: BackgroundType.Image,
+  };
+
+  if (imagePart.indexOf('url(') > -1) {
+    // Background is Image
+    result[PROPERTY_BG_IMAGE] = imagePart;
+  } else if (imagePart.indexOf('gradient(') > -1) {
+    const parsed = parseGradient(imagePart);
+    const { stops } = parsed;
+
+    if (
+      stops.length === 2
+      && parsed.type === 'linear'
+      && (stops[0].color === stops[1].color)
+    ) {
+      // Background is Color
+      result[PROPERTY_BG_TYPE] = BackgroundType.Color;
+      result[PROPERTY_BG_COLOR] = stops[0].color;
+    } else {
+      // Background is Gradient
+      const gradDir = getValidDir(parsed.direction) || GRAD_DIRS[0];
+      const gradType = parsed.type || GRAD_TYPES[0];
+      result[PROPERTY_BG_TYPE] = BackgroundType.Grad;
+      result[PROPERTY_BG_GRAD] = toGradient(gradType, gradDir, parsed.colors);
+      result[PROPERTY_BG_GRAD_TYPE] = gradType;
+      result[PROPERTY_BG_GRAD_DIR] = gradDir;
+    }
+  }
+  return result;
+};
+
 const bgTypeIconAttrs = 'style="max-height: 16px; display: block; margin: 0 auto" viewBox="0 0 24 24"';
 const PROPERTY_IMAGE = 'background-image';
 const PROPERTY_BG_TYPE = '__background-type';
 const PROPERTY_BG_IMAGE = PROPERTY_IMAGE;
 const PROPERTY_BG_COLOR = `${PROPERTY_BG_IMAGE}-color`;
 const PROPERTY_BG_GRAD = `${PROPERTY_BG_IMAGE}-gradient`;
+const PROPERTY_BG_GRAD_DIR = `${PROPERTY_BG_GRAD}-dir`;
+const PROPERTY_BG_GRAD_TYPE = `${PROPERTY_BG_GRAD}-type`;
+
+
 const DEFAULT_IMAGE = 'none';
 
 const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts = {}) => {
@@ -61,10 +98,15 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts = {}) => {
         if (
           (
             newTypeValue === BackgroundType.Image &&
-            [PROPERTY_BG_COLOR, PROPERTY_BG_GRAD].indexOf(propName) < 0
+            [PROPERTY_BG_COLOR, PROPERTY_BG_GRAD, PROPERTY_BG_GRAD_DIR, PROPERTY_BG_GRAD_TYPE].indexOf(propName) < 0
           )
-          || (newTypeValue === BackgroundType.Color && propName === PROPERTY_BG_COLOR)
-          || (newTypeValue === BackgroundType.Grad && propName === PROPERTY_BG_GRAD)
+          || (
+            newTypeValue === BackgroundType.Color && propName === PROPERTY_BG_COLOR
+          )
+          || (
+            newTypeValue === BackgroundType.Grad &&
+            [PROPERTY_BG_GRAD, PROPERTY_BG_GRAD_DIR, PROPERTY_BG_GRAD_TYPE].indexOf(propName) >= 0
+          )
         ) {
           visible = true;
         }
@@ -84,42 +126,13 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts = {}) => {
       return opt?.title ? `${capitalize(opt.title)}` : '';
     },
     fromStyle(style: Record<string, string>, { property, name }: any) {
-      const bg = style[name] || '';
       const sep = property.getLayerSeparator();
       const props = property.getProperties();
       let layers: any = [];
 
       if (style[PROPERTY_IMAGE]) {
-        // Get layers from the background-image property
-        layers = property.__splitStyleName(style, PROPERTY_IMAGE, sep)
-        .map((imagePart: string) => {
-          const result: Record<string, any> = {
-            [PROPERTY_BG_TYPE]: BackgroundType.Image,
-          };
-
-          if (imagePart.indexOf('url(') > -1) {
-            // Background is Image
-            result[PROPERTY_BG_IMAGE] = imagePart;
-          } else if (imagePart.indexOf('gradient(') > -1) {
-            const parsed = parseGradient(imagePart);
-            const { stops } = parsed;
-
-            if (
-              stops.length === 2
-              && parsed.type === 'linear'
-              && (stops[0].color === stops[1].color)
-            ) {
-              // Background is Color
-              result[PROPERTY_BG_TYPE] = BackgroundType.Color;
-              result[PROPERTY_BG_COLOR] = stops[0].color;
-            } else {
-              // Background is Gradient
-              result[PROPERTY_BG_TYPE] = BackgroundType.Grad;
-              result[PROPERTY_BG_GRAD] = imagePart;
-            }
-          }
-          return result;
-        });
+        // Get layers from the `background-image` property
+        layers = property.__splitStyleName(style, PROPERTY_IMAGE, sep).map(getLayerFromBgImage);
 
         // Update layers by inner properties
         props.forEach((prop: any) => {
@@ -132,6 +145,11 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts = {}) => {
               layers[i] = layers[i] ? { ...layers[i], ...inLayer } : inLayer;
             });
         });
+      } else if (style[name]) {
+        // Partial support for the `background` property
+        layers = property.__splitStyleName(style, name, /(?![^)(]*\([^)(]*?\)\)),(?![^\(]*\))/)
+          .map((value: string) => value.substring(0, value.lastIndexOf(')') + 1))
+          .map(getLayerFromBgImage);
       }
 
       return layers;
@@ -145,12 +163,19 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts = {}) => {
         const bgColor = values[PROPERTY_BG_COLOR];
         image = bgColor === DEFAULT_IMAGE ? DEFAULT_IMAGE : `linear-gradient(${bgColor} 0%, ${bgColor} 100%)`;
       } else if (type === BackgroundType.Grad) {
-        image = values[PROPERTY_BG_GRAD];
+        const parsed = parseGradient(values[PROPERTY_BG_GRAD] || '');
+        image = toGradient(
+          values[PROPERTY_BG_GRAD_TYPE] || GRAD_TYPES[0],
+          values[PROPERTY_BG_GRAD_DIR] || GRAD_DIRS[0],
+          parsed.colors
+        );
       }
 
       delete values[PROPERTY_BG_IMAGE];
       delete values[PROPERTY_BG_COLOR];
       delete values[PROPERTY_BG_GRAD];
+      delete values[PROPERTY_BG_GRAD_DIR];
+      delete values[PROPERTY_BG_GRAD_TYPE];
 
       return {
         ...values,
@@ -188,20 +213,6 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts = {}) => {
         default: 'none',
         functionName: 'url',
         type: 'file',
-        full: true,
-      },
-      {
-        label: 'Color',
-        property: PROPERTY_BG_COLOR,
-        default: 'none',
-        type: 'color',
-        full: true,
-      },
-      {
-        label: 'Gradient',
-        property: PROPERTY_BG_GRAD,
-        default: 'none',
-        type: 'gradient',
         full: true,
       },
       {
@@ -245,6 +256,34 @@ const plugin: grapesjs.Plugin<PluginOptions> = (editor, opts = {}) => {
         property: 'background-size',
         options: getOptions(['auto', 'cover', 'contain']),
       },
+      {
+        label: 'Color',
+        property: PROPERTY_BG_COLOR,
+        default: 'none',
+        type: 'color',
+        full: true,
+      },
+      {
+        label: 'Gradient',
+        property: PROPERTY_BG_GRAD,
+        default: 'linear-gradient(right, black 0%, white 100%)',
+        type: 'gradient',
+        full: true,
+      },
+      {
+        name: 'Direction',
+        property: PROPERTY_BG_GRAD_DIR,
+        type: 'select',
+        default: 'right',
+        options: getOptions(GRAD_DIRS),
+      },
+      {
+        name: 'Type',
+        property: PROPERTY_BG_GRAD_TYPE,
+        default: 'linear',
+        type: 'select',
+        options: getOptions(GRAD_TYPES),
+      }
     ].map(prop => options.propExtender?.(prop) || prop)
   });
 };
